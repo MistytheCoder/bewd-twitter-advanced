e45a38c5e09f027458a3ed5f3894fceeb0cbe075
h8rry <harrychenca@gmail.com>
2025-03-27 13:03:18 -0700

    Minor fix

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -6,11 +6,15 @@ class Tweet < ApplicationRecord
 
   has_one_attached :image
 
-  after_create :notify_via_email
+  after_commit :notify_via_email, on: :create
 
   private
 
   def notify_via_email
+    # Only send notification if the tweet was successfully created
+    return unless persisted?
+    # Ensure image is properly attached before sending
+    image.analyze if image.attached?
     TweetMailer.notify(self).deliver!
   end
 end
